a4bb755591af097f68b47fdd8f237d39b5c0dcf9
sara sallat <sallatsara@gmail.com>
2022-12-10 22:30:37 +0300

    completed assignment

---
+++ src/components/Filter.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Filter( {selectedCategory, onCategoryChange} ){
+
+    function handleCategoryChange(event) {
+        onCategoryChange(event.target.value);
+      }
+
+    return (
+        <div className="Filter">
+        <select name="filter" onChange={handleCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+    )
+}
+
+export default Filter;
